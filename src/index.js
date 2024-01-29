9dedd5542c15871710b09c9b4e9d917ae06ad212
charliewheels <149970734+charliewheels@users.noreply.github.com>
2024-01-29 16:43:36 +0000

    Added API for city search & temperature
    Now when you search for a city it pulls the correct temperature information

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "f01cdea927tb3faa4b57435de8b1eo83";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let citySearchFormElement = document.querySelector("#city-search-form");
 citySearchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
